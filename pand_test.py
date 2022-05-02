3177480dfecdd94f5c53ded7531d588a174853c7
Khmara Dmytro <khmara.dmytro@gmail.com>
2022-05-02 21:30:24 +0200

    addded pandas

--- pand_test.py
+++ pand_test.py
@@ -6,12 +6,12 @@ for df in pd.read_csv('pokemon_data.csv', chunksize=100):
     df['count']=1
     if i==0:
         new_df = pd.DataFrame()
-      #  new_df['count']=1
     i+=1
     print ('CHUNK #'+ str(i))
     results = df.groupby(['Type 1']).count()['Name']
     new_df = pd.concat([new_df, results])
-    print   (new_df)
-#new_df['0']=int(new_df['0'])
+##    print   (new_df)
+##new_df['0']=int(new_df['0'])
+
 print(new_df.groupby(level=0).sum())
 new_df.to_excel('new.xlsx')
